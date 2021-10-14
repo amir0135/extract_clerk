400ed4a08c9ec1a4f7e69ed29597645eec8ec505
Amira Moussa <amiramoussa36@gmail.com>
2021-10-14 14:36:47 +0200

    added instructions in main.py

--- scan.py
+++ scan.py
@@ -4,36 +4,40 @@ import pytesseract
 from wand.image import Image as wi
 
 ''''
-This file takes a scanned file and converts it to a txt file. 
+This file takes a scanned file and converts it to a txt file.
 Is pretty sensitive to dark parts in the scanned file
 '''
 
-pdfFile = wi(filename = "test_files/testnew.pdf", resolution = 300)
-image = pdfFile.convert('jpeg')
 
-imageBlobs = []
+def convert_scanfiles(file):
+	pdfFile = wi(filename = file, resolution = 300)
+	image = pdfFile.convert('jpeg')
 
-#save images for themselves
-for img in image.sequence:
-	imgPage = wi(image = img)
-	imageBlobs.append(imgPage.make_blob('jpeg'))
+	imageBlobs = []
 
-#extract text out
-extract = []
+	#save images for themselves
+	for img in image.sequence:
+		imgPage = wi(image = img)
+		imageBlobs.append(imgPage.make_blob('jpeg'))
 
-# for imgBlob in imageBlobs:
-for i in range(len(imageBlobs)):
-	image = Image.open(io.BytesIO(imageBlobs[i]))
-	text = pytesseract.image_to_string(image, lang = 'eng')
-	extract.append(text)
+	#extract text out
+	extract = []
 
-#write it to a txt file so we can load it in 'extract.py' and sort
-write_file = "batch1.txt"
-with open(write_file, "w") as output:
-    for line in extract:
-        output.write(line)
+	# for imgBlob in imageBlobs:
+	for i in range(len(imageBlobs)):
+		image = Image.open(io.BytesIO(imageBlobs[i]))
+		text = pytesseract.image_to_string(image, lang = 'eng')
+		extract.append(text)
+
+	#write it to a txt file so we can load it in 'extract.py' and sort
+	write_file = file.replace('.pdf','.txt')
+	with open(write_file, "w") as output:
+		for line in extract:
+			output.write(line)
+	return output
 
 
-import time
-start_time = time.time()
-print("--- %s seconds ---" % (time.time() - start_time))
+# convert_scanfiles('test_files/testnew.pdf')
+# import time
+# start_time = time.time()
+# print("--- %s seconds ---" % (time.time() - start_time))
