5946eebc44ec42abd122e6dbf01a2a09d9f6a7bb
Amira Moussa <amiramoussa36@gmail.com>
2022-01-02 23:30:09 +0100

    rewrote all code and fixed mistakes.

---
+++ library.py
@@ -0,0 +1,154 @@
+from os import name
+import numpy as np
+import pandas as pd
+
+
+def flat(list):
+    return [item for sublist in list for item in sublist]
+
+#extract index for places in text we are interessted in
+def extract_ind(lst, item):
+    return [i for i, x in enumerate(lst) if item in x]
+
+#### returns index of last element in list #####
+def last_index(lst):
+    last_ind = []
+    for i in range(len(lst)):
+        last_ind.append(len(lst[i])-1)
+    return last_ind
+
+
+def clean_text(data):
+    lst_ind = []
+    for i in range(len(data)):
+        if data[i] == '':
+            lst_ind.append(False)
+        else:
+            lst_ind.append(True)
+
+    new_data = []
+    for i in range(len(data)):
+        if lst_ind[i] == True:
+            new_data.append(data[i])
+        else:
+            continue
+
+
+
+
+    def index_order(data, start, end_ind, item):
+        #Delete unecessary index of the word 'Chambers' since this appear in places we are not interested in
+        start_ind = []
+        for i in range(len(start)):
+            if not item in data[start[i]]:
+                start_ind.append(start[i]) 
+
+        end_new = []
+        for i in start_ind:
+            for j in end_ind:
+                if j>i:
+                    end_new.append(j)
+                    break
+        if len(start_ind) > len(end_new):
+            #end_new.append(extract_ind(data, data[-1])[0])
+            end_new.append(len(new_data)-1)
+
+        staff_data = []
+        for j, k in zip(start_ind,end_new):
+            staff_data.append(data[j:k])
+
+        return staff_data
+
+    # start = extract_ind(new_data, 'Staff')
+    start = extract_ind(new_data, 'Chambers of')
+    end = start[1:]
+    #end.append(start[-1])
+    staff_data_old = index_order(new_data, start, end, 'continued')
+
+    staff_data = []
+    for i in range(len(staff_data_old)):
+        if staff_data_old[i][0].startswith('Chambers of'):
+            staff_data.append(staff_data_old[i])
+        elif not staff_data_old[i][0].startswith('Chambers of') and 'Chambers' in staff_data_old[i][0]:
+            staff_data_old[i][0] = 'Chambers' + staff_data_old[i][0].split('Chambers')[-1]
+            staff_data.append(staff_data_old[i])
+        else:
+            staff_data.append(staff_data_old[i-1]+ staff_data_old[i])
+
+    def name_data_extraction(data):
+        name_data = []
+        for i in range(len(data)):
+            if 'continued' in data[i][0]:
+                pass
+            if 'Chambers of' in data[i][0] and 'Judge' in data[i][0]:
+                name_data.append(data[i][0].split("Judge")[1]) 
+            elif 'Chambers of' in data[i][0] and 'Justice' in data[i][0]:
+                name_data.append(data[i][0].split("Justice")[1])
+        name_data = list(dict.fromkeys(name_data))
+        return name_data
+    
+    save_name = name_data_extraction(staff_data) 
+    test= []
+    name_data = []
+    for j in save_name:
+        for i in range(len(staff_data)-1):
+            if j in staff_data[i][0] and staff_data[i][0] not in staff_data[i+1][0]:
+                test.append(staff_data[i])
+                name_data.append(j)
+                save_name = save_name[1:]
+                break
+            else:
+                continue 
+
+    return test, name_data
+
+    #### exttracting names judge ###########
+
+
+def word_ind(data, item):
+    word_start = []
+    for i in range(len(data)):
+        if not [s for s in data[i] if item in s]:
+            word_start.append(['N/A'])
+        else:
+            word_start.append(extract_ind(data[i],item))
+
+        #flatten list
+        if len(word_start[i]) > 1:
+            word_start[i] = [word_start[i][0]] #delete if career pops up more times to have equal sizes
+    word_start = flat(word_start)
+    return word_start
+
+''''
+    takes in index from first word and index from last word-
+    will extract information either from the first index to last index,
+    only information from first index or only info from last index depending of existince of the
+    words in each list
+
+    input: data, start word index, end word index
+    output: List of lists containing information from word or N/A
+
+    example all information from the word 'education' all the way to the word 'career'
+
+'''
+def extract_information(data, start_ind, end_ind):
+    start_data = []
+    start_ny = []
+    for i, j, k in zip(range(len(data)), start_ind, end_ind):
+        #for i in range(len(data)):
+        if j == 'N/A' and k == 'N/A':
+            start_data.append('N/A')
+        elif j == 'N/A':
+            start_data.append('N/A')
+        elif k == 'N/A':
+            if j == len(data[i]):
+                start_data.append(data[i][j:-1])
+            else:
+                start_data.append([data[i][j]])
+        else:
+            start_data.append(data[i][j:k])
+
+        start_ny.append(' '.join(start_data[i]))
+
+    return start_ny
+
--- main.py
+++ main.py
@@ -2,37 +2,131 @@ from new_extract import extract_clerk_info
 from scan import convert_scanfiles
 import os
 from judge import extract_judge_info
+import PyPDF2
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import numpy as np
+import shutil
+
+'''
+READ ME:
+
+    This is the main program which loops over the pdf files and creates a folder for each batch file.
+
+    This program consists of 4 files:
+    scan.py - this is the file that reads the scanned files and converts them to txt files
+    judge.py - this is the file that reads the txt files and extracts the judge information
+    new_extract.py - this is the file that reads the txt files and extracts the clerk information
+
+    main.py - this is the main file that runs the program and you have to insert the directory of the scanned files and the directory of where you want to save the files.
+'''
+
+
+def make_folder(folder_dir, dir_save):
+    folder_list = os.listdir(folder_dir)
+    folder_list = list(np.asarray(folder_list))
+    folder_list.sort()
+    folder_list.pop(0)
+    for i in folder_list:
+        if not os.path.exists(dir_save + '/' + i.split('.pdf')[0]):
+            os.makedirs(dir_save + '/' + i.split('.pdf')[0])
+    return folder_list
+
+def split_pdf(directory, folder_list):
+    for folder_name in folder_list:
+        folder_path = directory + '/' + folder_name
+        for filename in os.listdir(directory):
+            if filename == folder_name + '.pdf':
+                f = os.path.join(directory, filename)
+                pdf_reader = PdfFileReader(open(f, 'rb'))
+                for page in range(pdf_reader.numPages):
+                    pdf_writer = PdfFileWriter()
+                    pdf_writer.addPage(pdf_reader.getPage(page))
+                    pdf_output = open(folder_path + '/' + '%s_page%s.pdf' % (filename.split('.')[0], page), 'wb')
+                    pdf_writer.write(pdf_output)
+                    pdf_output.close()
+    return
+
+def convert_files(dir, folder_list):
+    folder_list.pop(0)
+    for folder_name in folder_list:
+        folder_path = dir + '/' + folder_name
+        path = os.listdir(folder_path)
+        path.sort()
+        for filename in path:
+            for i in range(0,len(path),2):
+                if i == -1:
+                    break
+                elif path[i].split('.')[0] == path[i+1].split('.')[0]:
+                    pass
+                else:
+                    f = os.path.join(folder_path, filename)
+                    convert_scanfiles(f)
+    return
+
+def concatFiles(dir, save_dir, folder_list):
+    for folder_name in folder_list:
+        folder_path = dir + '/' + folder_name
+        path = os.listdir(folder_path)
+        path = sorted(path, key = lambda x: x.rsplit('page',1)[1])
+        path.sort(key = len)
+        with open(os.path.join(save_dir ,folder_name +'/' + folder_name + '.txt'), "wb") as fo:
+            for filename in path:
+                if filename.endswith('.txt'):
+                    with open(os.path.join(folder_path, filename), "rb") as fi:
+                        shutil.copyfileobj(fi, fo)
+    return
+
+def extract_data(dir, folder_list):
+    for folder_name in folder_list:
+        folder_path = dir + '/' + folder_name
+        path = os.listdir(folder_path)
+        path.sort()
+        for filename in path:
+            # if len([s for s in path if "csv" in s]) >= 2:
+            #     break
+            if filename.endswith('.txt'):
+                f = os.path.join(folder_path, filename)
+                extract_clerk_info(f)
+                extract_judge_info(f)
+            else:
+                pass
+    return
+
+
+###### MAIN #########
+
+def main(dir, save_dir):
+    # folder_list = make_folder(dir, dir)
+    # new_folder_list = []
+    # for i in folder_list:
+    #     if not i.endswith('.pdf'):
+    #         new_folder_list.append(i)
+    # new_folder_list.sort()
+    # split_pdf(dir, new_folder_list)
+    # convert_files(dir, new_folder_list)
+
+    folder_list = make_folder(dir, save_dir)
+    new_folder_list = []
+    for i in folder_list:
+        if not i.endswith('.pdf'):
+            new_folder_list.append(i)
+    new_folder_list.sort()
+    #concatFiles(dir, save_dir, new_folder_list)
+    extract_data(save_dir, new_folder_list)
+    return
 
 ''''
-Run over all batch files
+
+MAIN PROGRAM
+
+Run over all batch files DOWN BELOW
 STEP 1: Write the directory to the folder for the pdf files
-STEP 2: run the first part to convert from scan to txt
-STEP 3: Run to extract information and save as a csv file
+    dir = folder with all the pdf files
+    save_dir = folder to save the txt files
 
 '''
 
-def convert_files(dir):
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        ##checking if it is a file
-        if os.path.isfile(f) and filename.endswith('.pdf') :
-            convert_scanfiles(f)
-    return
+dir = '/Users/Amira/Desktop/data_extract/textbooks'
+save_dir = '/Users/Amira/Desktop/data_extract/extract_books'
 
-def extract_data(dir):
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        if filename.endswith('.txt'):
-            extract_clerk_info(f)
-            extract_judge_info(f)
-    return
-
-####### STEP 1 #######
-directory = '/Users/Amira/Desktop/data_extract/test_files'
-
-####### STEP 2 ####### 
-#run step 2 and 3 seperatly, since step 2 takes very long and only needs to be done once
-convert_files(directory)
-
-####### STEP 3 #######
-extract_data(directory)
+main(dir, save_dir)
